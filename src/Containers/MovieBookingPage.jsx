3055486c2e356d8d058d11e5888d781fa16fbd05
yvnb <yvnagendrababu@gmail.com>
2022-06-12 14:19:40 +0400

    Movie Booking Form with Formik Added

--- src/Containers/MovieBookingPage.jsx
+++ src/Containers/MovieBookingPage.jsx
@@ -1,14 +1,15 @@
 import { Form, Formik } from "formik";
 import * as Yup from "yup";
+import { MovieBookingForm } from "../Components/MovieBookingForm";
 import { TextField } from "../Components/TextField";
-import UserForm from "../Components/UserForm";
+
 
 
 export default function MovieBookingPage() {
   
   return ( 
     <div>
-      <UserForm />
+      <MovieBookingForm />
     </div>
   );
 }
