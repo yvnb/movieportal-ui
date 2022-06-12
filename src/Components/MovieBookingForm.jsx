9394ca1deb106688ce3c2250a85789f0aadc8518
yvnb <yvnagendrababu@gmail.com>
2022-06-13 03:46:52 +0400

    Manage Booking page with edit functionality

---
+++ src/Components/MovieBookingForm.jsx
@@ -0,0 +1,151 @@
+import React, { useState} from 'react';
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Grid, Typography } from "@material-ui/core";
+import DateTimePicker from "./DateTimePicker";
+import Textfield from "./TextField";
+import Button from "./Button";
+import axios from "axios";
+import { Alert } from '@material-ui/lab';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  formWrapper: {
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(8),
+  },
+}));
+
+const BOOKING_FORM_INITIAL_STATE = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+  bookingDate: "",
+  seats: "",
+};
+
+const BOOKING_FORM_VALIDATION = Yup.object().shape({
+  firstName: Yup.string().required("Required"),
+  lastName: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email.").required("Required"),
+  phone: Yup.number()
+    .integer()
+    .typeError("Please enter a valid phone number")
+    .required("Required"),
+  seats: Yup.number()
+    .integer()
+    .typeError("Please enter Valid Seats")
+    .min(1, "Min value 1")
+    .max(10, "Max value 10")
+    .required("Required"),
+  bookingDate: Yup.date().required("Required"),
+});
+
+export function MovieBookingForm(){
+  const classes = useStyles();
+    //for error handling
+    const [iserror, setIserror] = useState(false)
+    const [errorMessages, setErrorMessages] = useState([])
+  
+    //for success updates
+    const [isSuccess, setIsSuccess] = useState(false)
+    const [successMessages, setSuccessMessages] = useState([])
+
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <Container maxWidth="md">
+          <div className={classes.formWrapper}>
+            <Formik
+              initialValues={{
+                ...BOOKING_FORM_INITIAL_STATE,
+              }}
+              validationSchema={BOOKING_FORM_VALIDATION}
+              onSubmit={(values) => {
+                console.log(JSON.stringify(values, null, 2));
+                const bookingData = JSON.stringify(values, null, 2);
+                axios
+                  .post("http://localhost:8080/api/booking", bookingData, {
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                  })
+                  .then(
+                    (response) => {
+                      setIserror(false)
+                      setErrorMessages([])
+                      setIsSuccess(true)
+                      setSuccessMessages(["Booking Created successfully !!!"])
+                    },
+                    (error) => {
+                      setIserror(true)
+                      setErrorMessages(["Booking failed due to server error. Please try later"])
+                      setIsSuccess(false)
+                      setSuccessMessages([])
+                    }
+                  );
+              }}
+            >
+              <Form>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                  <div>
+            {iserror && 
+              <Alert severity="error">
+                  {errorMessages.map((msg, i) => {
+                      return <div key={i}>{msg}</div>
+                  })}
+              </Alert>
+            }       
+          </div>
+          <div>
+            {isSuccess && 
+              <Alert severity="success">
+                  {successMessages.map((msg, i) => {
+                      return <div key={i}>{msg}</div>
+                  })}
+              </Alert>
+            }       
+          </div>
+                    <Typography>Booking details</Typography>
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <Textfield name="firstName" label="First Name" />
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <Textfield name="lastName" label="Last Name" />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Textfield name="email" label="Email" />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Textfield name="phone" label="Phone" />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <DateTimePicker name="bookingDate" label="Booking Date" />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Textfield name="seats" label="Number Of Seats" />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Button>Book Now</Button>
+                  </Grid>
+                </Grid>
+              </Form>
+            </Formik>
+          </div>
+        </Container>
+      </Grid>
+    </Grid>
+  );
+};
--- src/Containers/ManageBookingPage.jsx
+++ src/Containers/ManageBookingPage.jsx
@@ -1,21 +1,186 @@
-import {Component} from 'react';
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+import { forwardRef } from 'react';
+import Avatar from 'react-avatar';
+import Grid from '@material-ui/core/Grid'
 
-class ManageBookingPage extends Component {
+import MaterialTable from "material-table";
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import axios from 'axios'
+import Alert from '@material-ui/lab/Alert';
 
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
+
+
+function validateEmail(email){
+  const emailRegex = /^((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))$/;
+  return emailRegex.test(String(email).toLowerCase());
+}
+
+function isNumber(field){
+    const digitRegex = /^\d+\.?\d*$/;
+    return digitRegex.test(String(field));
+}
+
+export default function ManageBookingPage() {
+
+  var columns = [
+    {title: "id", field: "id", hidden: true},    
+    {title: "First Name", field: "firstName"},
+    {title: "Last Name", field: "lastName"},
+    {title: "Email", field: "email"},
+    {title: "Phone", field: "phone"},
+    {title: "Seats", field: "seats"},
+    {title: "Booking Date", field: "bookingDate"},
+    {title: "Booking Status", field: "bookingStatus"},
+    
+  ]
+  const [data, setData] = useState([]); //table data
+
+  //for error handling
+  const [iserror, setIserror] = useState(false)
+  const [errorMessages, setErrorMessages] = useState([])
+
+  //for success updates
+  const [isSuccess, setIsSuccess] = useState(false)
+  const [successMessages, setSuccessMessages] = useState([])
+
+  useEffect(() => { 
+    axios.get(`http://localhost:8080/api/booking`) 
+        .then(res => {          
+            setData(res.data)
+         })
+         .catch(error=>{
+          setErrorMessages(["Not Able to fetch Booking Details. Please try again later"])
+          setIserror(true)
+         })
+  }, [])
+
+  const handleRowUpdate = (newData, oldData, resolve) => {
+    //validation
+    let errorList = []
+    if(newData.firstName === ""){
+      errorList.push("Please enter first name")
     }
+    if(newData.lastName === ""){
+      errorList.push("Please enter last name")
+    }
+    if(newData.email === "" || validateEmail(newData.email) === false){
+      errorList.push("Please enter a valid email")
+    }
+    if(newData.phone === "" || isNumber(newData.phone) === false){
+        errorList.push("Please enter a valid phone number")
+    }
+    if(newData.seats === "" || isNumber(newData.seats) === false){
+        errorList.push("Please enter valid seats")
+    }  
+    if(newData.bookingDate === "" ){
+        errorList.push("Please enter a valid booking date")
+    }  
 
-    render(){
+    if(errorList.length < 1){
+      console.log("getting the booking id " + newData.id);  
+      axios.put("http://localhost:8080/api/booking/"+newData.id, newData)
+      .then(res => {
+        const dataUpdate = [...data];
+        const index = oldData.tableData.id;
+        dataUpdate[index] = newData;
+        setData([...dataUpdate]);
+        resolve()
+        setIserror(false)
+        setErrorMessages([])
+        setIsSuccess(true)
+        setSuccessMessages(["Booking updated successfully !!!"])
+      })
+      .catch(error => {
+        setErrorMessages(["Update failed! Server error"])
+        setIserror(true)
+        setIsSuccess(false)
+        resolve()
         
-        return (
-            <div>
-                <h1>"This is for Manage Booking Page"</h1>
-            </div>
-        );
-    } 
-} 
+      })
+    }else{
+      setErrorMessages(errorList)
+      setIserror(true)
+      setIsSuccess(false)
+      resolve()
 
-export default ManageBookingPage;
+    }
+    
+  }
+
+  return (
+    <div >
+      
+      <Grid container spacing={1}>
+          <Grid item xs={3}></Grid>
+          <Grid item xs={12}>
+          <div>
+            {iserror && 
+              <Alert severity="error">
+                  {errorMessages.map((msg, i) => {
+                      return <div key={i}>{msg}</div>
+                  })}
+              </Alert>
+            }       
+          </div>
+          <div>
+            {isSuccess && 
+              <Alert severity="success">
+                  {successMessages.map((msg, i) => {
+                      return <div key={i}>{msg}</div>
+                  })}
+              </Alert>
+            }       
+          </div>
+            <MaterialTable
+              title="Manage Bookings"
+              columns={columns}
+              data={data}
+              icons={tableIcons}
+              editable={{
+                onRowUpdate: (newData, oldData) =>
+                  new Promise((resolve) => {
+                      handleRowUpdate(newData, oldData, resolve);
+                      
+                  }),
+              }}
+            />
+          </Grid>
+          <Grid item xs={6}></Grid>
+        </Grid>
+    </div>
+  );
+}
